5cdf4f33cb83e636abe4d388357f4d5f80a6ad45
KiranR-max <141916203+Kiran-KumarR@users.noreply.github.com>
2024-08-26 21:30:32 +0530

    cleanup main branch

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,16 +6,7 @@ const config: Config = {
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
-  theme: {
-    container: {
-      center: true,
-      padding: {
-        DEFAULT: '1rem',
-        md: '1.5rem',
-        lg: '2rem'
-      }
-    }
-  },
+  theme: {},
   plugins: []
 }
 export default config
